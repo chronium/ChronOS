3b659d8c1b265fd9c2b1774fc2bddc3c7f020a60
Chronium <onlivechronium@gmail.com>
2016-05-24 08:05:17 +0000

    Fixed spacing.

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -72,9 +72,9 @@ void term_writes (const char *data) {
 void term_set_cursor (size_t x, size_t y) {
 	uint16_t pos = x + y * VGA_WIDTH;
 
-    outb(0x3D4, 0x0F);
-    outb(0x3D5, (uint8_t) (pos & 0xFF));
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (uint8_t) (pos & 0xFF));
 
-    outb(0x3D4, 0x0E);
-    outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
 }
