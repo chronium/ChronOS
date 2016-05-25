a19f5f1c92c3193f9993af12f3ccd52054b4843c
Chronium <onlivechronium@gmail.com>
2016-05-25 08:38:04 +0000

    Added platform checks in libc.

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -1,9 +1,11 @@
 #include <string.h>
 
 void *memcpy (void *__restrict dest, const void *__restrict src, size_t n) {
+#if defined (__i386__)
 	asm volatile ("rep movsb"
 				: "=c" ((int) { 0 })
 				: "D" (dest), "S" (src), "c" (n)
 				: "flags", "memory");
+#endif
 	return dest;
 }
--- libc/string/memset.c
+++ libc/string/memset.c
@@ -1,9 +1,11 @@
 #include <string.h>
 
 void *memset (void *dest, int c, size_t n) {
+#if defined (__i386__)
 	asm volatile ("rep stosb"
 				: "=c" ((int) { 0 })
 				: "D" (dest), "a" (c), "c" (n)
 				: "flags", "memory");
+#endif
 	return dest;
 }
